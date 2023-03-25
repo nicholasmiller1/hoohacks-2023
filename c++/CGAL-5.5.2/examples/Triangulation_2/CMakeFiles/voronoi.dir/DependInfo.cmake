196428756e6d67955d007651bf8c428f791c7e00
John-Zoscak-02 <jzoscak02@gmail.com>
2023-03-25 14:08:43 -0400

    Attempt building cgal example code

---
+++ c++/CGAL-5.5.2/examples/Triangulation_2/CMakeFiles/voronoi.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/Student/Documents/Git/hoohacks-2023/c++/CGAL-5.5.2/examples/Triangulation_2/voronoi.cpp" "CMakeFiles/voronoi.dir/voronoi.cpp.o" "gcc" "CMakeFiles/voronoi.dir/voronoi.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
